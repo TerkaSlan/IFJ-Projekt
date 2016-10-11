c331ddf0d4a518a4661df9bdc52622a42057502a
“Katarina <xgreso00@stud.fit.vutbr.cz>
2016-10-11 11:59:00 +0200

    [TAKS #2] functions newToken and freeToken

--- token.c
+++ token.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <inttypes.h>
+#include "str.h"
 #include "token.h"
+#include "stdio.h"
+
+Token* newToken() {
+	Token *token = calloc(1, sizeof(Token));
+	if (token == NULL) {
+		//TODO: hadle error
+	} else {
+		token->type = TT_empty;
+	}
+
+	return token;
+}
+
+void freeToken(Token **pToken) {
+	if (pToken == NULL) {
+		//TODO: hadle error
+	} else {
+		if (((*pToken)->type == TT_variable)
+        	|| ((*pToken)->type == TT_identifier)
+			|| ((*pToken)->type == TT_string)) { //here have to be all TokenTypes which contains String data
+			strFree(&(*pToken)->str);
+		}
+		free(*pToken);
+		*pToken = NULL;
+	}
+
+}
+
+
--- token.h
+++ token.h
@@ -1,4 +1,66 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include "str.h"
+
+typedef enum {
+
+	TT_empty,
+	TT_variable,
+	TT_identifier,
+	TT_string,
+	TT_double,
+	TT_number,
+	TT_keyword
+	//TODO: add all possible token types
+
+} TokenType;
+
+typedef enum {
+
+	KTT_boolean,
+	KTT_break,
+	KTT_class,
+	KTT_continue,
+	KTT_do,
+	KTT_double,
+	KTT_else,
+	KTT_false,
+	KTT_for,
+	KTT_if,
+	KTT_int,
+	KTT_return,
+	KTT_String,
+	KTT_static,
+	KTT_true,
+	KTT_void,
+	KTT_while
+
+} KeywordTokenType;
+
+typedef struct {
+
+    TokenType type;
+    union {
+        double dNum;
+        int32_t iNum;
+        KeywordTokenType keywordType;
+        dtStr str;
+    };
+
+} Token;
+
+/**
+ * Create new token and initializes it to empty state
+ * 
+ */
+Token* newToken();
+
+/**
+ * Deallocates memory of token's content if needed, deallocates token and sets pointer to NULL.
+ *  
+ * @param pToken token to free
+ */
+void freeToken(Token **pToken);
+
 #endif
