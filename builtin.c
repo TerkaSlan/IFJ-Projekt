cb440e136d6d9fdf68311203d9850a26a049d462
Jakub Handzus <xhandz00@stud.fit.vutbr.cz>
2016-11-17 19:13:11 +0100

    [#12][FIX] Internal merge conflict

--- builtin.c
+++ builtin.c
@@ -6,27 +6,28 @@
 
 eError substr(const dtStr *s, int32_t beginIndex, int32_t endIndex, dtStrPtr *subStr) {
 	if (s == NULL || s->str == NULL) {
-		printError(ERR_OTHER, "Parameter or its member is NULL");
-		return ERR_OTHER;
+		printError(ERR_INTERN, "In substr: Parameter or its member is NULL\n");
+		return ERR_INTERN;
 	}
 
 	if (endIndex > s->uiLength) {
-		printError(ERR_OTHER, "String index is out of range");
+		printError(ERR_OTHER, "In substr: String index is out of range\n");
 		return ERR_OTHER;
 	}
 	
 	if (beginIndex > endIndex) {
-		printError(ERR_OTHER, "String index is out of range");
+		printError(ERR_OTHER, "In substr: String index is out of range\n");
 		return ERR_OTHER;
 	}
 
 	if (beginIndex < 0) {
-		printError(ERR_OTHER, "String index is out of range");
+		printError(ERR_OTHER, "In substr: String index is out of range\n");
 		return ERR_OTHER;
 	}
 
 	*subStr = strNew();
 	if (subStr == NULL) {
+		printError(ERR_INTERN, "In substr: Cannot create string. Out of memory.\n");
 		return ERR_INTERN;
 	}
 	for (uint32_t i = beginIndex; i < endIndex; i++) {
@@ -46,7 +47,7 @@ eError readData(tSymbolPtr symbol, tSymbolData* data) {
 				tmp = getchar();
 			}		
 			if (next != '\n' && next != EOF) {
-				printError(ERR_RUN_INPUT, "Unexpected data (expected integer)");
+				printError(ERR_RUN_INPUT, "Error while reading from stdin: unexpected data (expected integer)\n");
 				return ERR_RUN_INPUT;
 			}
 			break;
@@ -58,13 +59,14 @@ eError readData(tSymbolPtr symbol, tSymbolData* data) {
 				tmp = getchar();
 			}
 			if (next != '\n'&& next != EOF) {
-				printError(ERR_RUN_INPUT, "Unexpected data (expected double)");				
+				printError(ERR_RUN_INPUT, "Error while reading from stdin: unexpected data (expected double)\n");				
 				return ERR_RUN_INPUT;
 			}
 			break;
 
 		case eSTRING:
 			if ((data->String = strNew()) == NULL) {
+				printError(ERR_INTERN, "In readData: Cannot create string. Out of memory.\n");
 				return ERR_INTERN;
 			}
 			while ((c = getc(stdin)) != EOF && c != '\n') {
@@ -73,7 +75,7 @@ eError readData(tSymbolPtr symbol, tSymbolData* data) {
 			break;
 
 		default:
-			printError(ERR_RUN_INPUT, "Unexpected data");
+			printError(ERR_RUN_INPUT, "Error while reading from stdin: unexpected data\n");
 			return ERR_RUN_INPUT;
 	}
 	return ERR_OK;
